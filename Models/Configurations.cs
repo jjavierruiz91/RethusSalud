82f6fa5a8f78806476dcb46dda81158ed6c5832f
Alvis <aherrera@munily.com>
2023-03-30 21:04:41 -0500

    feat: add model configuration

---
+++ Models/Configurations.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rethus_backend.Models
+{
+  public class Configurations
+  {
+    public int UserId { get; set; }
+
+    public User? User { get; set; }
+
+    public string? state { get; set; }
+
+    public string? step { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+  }
+}
