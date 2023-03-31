a41d8223d024776a225dac38f03495eb3ff91a60
Alvis <aherrera@munily.com>
2023-03-30 21:46:16 -0500

    feat: updates db context

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -15,10 +15,22 @@ namespace rethus_backend.Data
     }
 
     public DbSet<User> Users { get; set; }
+    public DbSet<Configurations> Configurations { get; set; }
+    public DbSet<UserForm> UserForm { get; set; }
 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+      modelBuilder.Entity<User>().ToTable("User");
+      modelBuilder.Entity<Configurations>().ToTable("Configurations");
+      modelBuilder.Entity<UserForm>().ToTable("UserForm");
+
+      modelBuilder.Entity<Configurations>()
+        .HasNoKey();
+
+      modelBuilder.Entity<UserForm>()
+        .HasNoKey();
+
       base.OnModelCreating(modelBuilder);
     }
 
