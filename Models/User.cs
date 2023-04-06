b42c0d67b5233e15eeafed1445a01523aee3f54a
Alvis <aherrera@munily.com>
2023-04-06 08:44:16 -0500

    feat: update user model

--- Models/User.cs
+++ Models/User.cs
@@ -6,14 +6,16 @@ namespace rethus_backend.Models
   public class User
   {
 
+    public string? UserId { get; set; }
     public string? email { get; set; }
-
-    [Key]
-    public string? identification { get; set; }
     public string? status { get; set; }
     public string? Roles { get; set; }
     public string? Token { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
+
+    public Configurations? Configurations { get; set; }
+
+    public UserForm? UserForm { get; set; }
   }
 }
