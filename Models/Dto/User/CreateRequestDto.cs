763d1c42479adcb2cc2ce0b580ad6cf7ad866ed7
Alvis <aherrera@munily.com>
2023-04-08 14:32:35 -0500

    feat: updated user dto

--- Models/Dto/User/CreateRequestDto.cs
+++ Models/Dto/User/CreateRequestDto.cs
@@ -2,7 +2,9 @@ namespace rethus_backend.Models.Dto.User
 {
   public class CreateRequestDto
   {
-    public string? Identification { get; set; }
+    // public string? Identification { get; set; }
     public string? email { get; set; }
+    public string? name { get; set; }
+    public string? password { get; set; }
   }
 }
