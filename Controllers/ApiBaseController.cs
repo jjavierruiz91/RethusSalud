f73b34f1d44dc315796c54b4fa8ac472695f65c4
Alvis <aherrera@munily.com>
2023-04-08 14:30:05 -0500

    feat: update api base

--- Controllers/ApiBaseController.cs
+++ Controllers/ApiBaseController.cs
@@ -2,19 +2,20 @@
 using Microsoft.AspNetCore.Mvc;
 using rethus_backend.Models;
 using rethus_backend.Repository.IRepository;
-using Microsoft.AspNetCore.Http;
+using AutoMapper;
 
 namespace rethus_backend.Controllers;
 
 [ApiController]
-[Route("[controller]")]
 public class ApiBaseController : ControllerBase
 {
-  protected readonly IUnitOfWork _unitOfWork = null!;
+  [Inject] protected readonly IMapper _mapper = null!;
+  [Inject] protected readonly IUnitOfWork _unitOfWork = null!;
   protected readonly ApiResponse _response;
 
   public ApiBaseController(IServiceProvider provider)
   {
+    provider.Inject(this);
     _response = new ApiResponse();
   }
 }
