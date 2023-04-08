bc8487ac45c588bb1debcfbcceafe727061be034
Alvis <aherrera@munily.com>
2023-04-08 14:31:54 -0500

    feat: updated apiResponse

--- Models/ApiResponse.cs
+++ Models/ApiResponse.cs
@@ -4,9 +4,21 @@ namespace rethus_backend.Models
 {
   public class ApiResponse
   {
-    public HttpStatusCode StatusCode { get; set; }
+    public HttpStatusCode StatusCode { get; set; } = HttpStatusCode.OK;
     public bool IsSuccess { get; set; } = true;
     public List<string> Messages { get; set; } = new List<string>();
-    public object Result { get; set; }
+    public object Result { get; set; } = "";
+
+    public ApiResponse(ApiResponse response)
+    {
+      this.StatusCode = response.StatusCode;
+      this.IsSuccess = response.IsSuccess;
+      this.Messages = response.Messages;
+      this.Result = response.Result;
+    }
+
+    public ApiResponse()
+    {
+    }
   }
 }
