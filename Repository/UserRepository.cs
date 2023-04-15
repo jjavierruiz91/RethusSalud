81df5b5e381c03c5c0f3bd0860f21dbc7dc880be
Alvis <aherrera@munily.com>
2023-04-14 22:06:29 -0500

    feat: update user

--- Repository/UserRepository.cs
+++ Repository/UserRepository.cs
@@ -2,7 +2,7 @@ using rethus_backend.Data;
 using rethus_backend.Models;
 using rethus_backend.Models.Dto.User;
 using rethus_backend.Repository.IRepository;
-
+using rethus_backend.Utilities.Security.Hashing;
 namespace rethus_backend.Repository
 {
   public class UserRepository : Repository<User>, IUserRepository
@@ -40,11 +40,15 @@ namespace rethus_backend.Repository
 
     public async Task<User> Register(CreateRequestDto createRequestDto)
     {
+      byte[] passwordHash, passwordSalt;
+      HashingHelper.CreatePasswordHash(createRequestDto.password, out passwordHash, out passwordSalt);
+
       User newUser = new()
       {
         name = createRequestDto.name,
         email = createRequestDto.email,
-        password = createRequestDto.password,
+        PasswordHash = passwordHash,
+        PasswordSalt = passwordSalt,
         Roles = "user",
         status = "active",
         Token = "",
