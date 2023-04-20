1e80ba810d4b5153d35c77c0c504608269dbdde7
Alvis <aherrera@munily.com>
2023-04-19 22:25:35 -0500

    feat: add userForm

--- Repository/User/UserFormRepository.cs
+++ Repository/User/UserFormRepository.cs
@@ -7,13 +7,33 @@ namespace rethus_backend.Repository
 {
   public class UserFormRepository : Repository<UserForm>, IUserFormRepository
   {
+    private readonly ApplicationDbContext _context;
+
     public UserFormRepository(ApplicationDbContext db) : base(db)
     {
+      _context = db;
+    }
+
+    public IEnumerable<UserForm> GetAll()
+    {
+      throw new NotImplementedException();
+    }
+
+    public UserForm GetById(string id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public bool IsExistUser(string userFormId)
+    {
+      throw new NotImplementedException();
     }
 
     public bool IsUniqueUser(string userId)
     {
-      throw new NotImplementedException();
+      UserForm user = _context.UserForm.FirstOrDefault(x => x.PersonalEmail == userId);
+      if (user == null) return false;
+      return true;
     }
 
     public Task<ApiResponse> post(UserFormCreateDto createRequestDto)
