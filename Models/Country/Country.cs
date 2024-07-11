08fce1202577ef6644acffd9cf4e2d0c24ca1e34
Alvis <aherrera@munily.com>
2024-07-10 21:52:21 -0500

    feat: add model of country

---
+++ Models/Country/Country.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace rethus_backend.Models
+{
+    public class Country
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string CountryId { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        [Required]
+        public required string Code { get; set; }
+
+        [Required]
+        public required string Capital { get; set; }
+
+        [Required]
+        public required string Region { get; set; }
+
+        [Required]
+        public required string Flag { get; set; }
+
+        [Required]
+        public required string DiallingCode { get; set; }
+
+        [Required]
+        public required string IsoCode { get; set; }
+
+        [Required]
+        public required int Population { get; set; }
+    }
+}
