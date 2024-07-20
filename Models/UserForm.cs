c3749e5bf1f66c381181179a4a6f2f1976b04477
Alvis <aherrera@munily.com>
2024-07-20 17:11:52 -0500

    feat: update type of field

--- Models/UserForm.cs
+++ Models/UserForm.cs
@@ -28,8 +28,8 @@ namespace rethus_backend.Models
         public City MunicipalityResidence { get; set; }
         public required string PersonalAddress { get; set; }
         public required DateTime DateBirth { get; set; }
-        public required int PersonalTelephone { get; set; }
-        public required int PersonalPhone { get; set; }
+        public required string PersonalTelephone { get; set; }
+        public required string PersonalPhone { get; set; }
         public required string PersonalEmail { get; set; }
         public required TypeEthnicGroup PersonalEthnicGroup { get; set; }
         public required string AcademicsOriginTitle { get; set; }
