e8009ab52ae8e194c3860f5ab140742409acb295
Alvis <aherrera@munily.com>
2024-07-20 20:21:36 -0500

    feat: update dto of userForm

--- Models/Dto/UserForm/UserFormCreateDto.cs
+++ Models/Dto/UserForm/UserFormCreateDto.cs
@@ -138,13 +138,13 @@ namespace rethus_backend.Models.Dto.UserForm
         public string? PersonalIdentification { get; set; }
         public string? PersonalFirstName { get; set; }
         public string? PersonalLastName { get; set; }
-        public int? PersonalCountryBirth { get; set; }
-        public int? PersonalDepartmentBirth { get; set; }
-        public int? PersonalMunicipalityBirth { get; set; }
+        public string? PersonalCountryBirth { get; set; }
+        public string? PersonalDepartmentBirth { get; set; }
+        public string? PersonalMunicipalityBirth { get; set; }
         public DateTime? DateBirth { get; set; }
-        public int? PersonalPlaceResidence { get; set; }
-        public int? PersonalDepartmentResidence { get; set; }
-        public int? PersonalMunicipalityResidence { get; set; }
+        public string? PersonalPlaceResidence { get; set; }
+        public string? PersonalDepartmentResidence { get; set; }
+        public string? PersonalMunicipalityResidence { get; set; }
         public string? PersonalAddress { get; set; }
         public string? PersonalTelephone { get; set; }
         public string? PersonalPhone { get; set; }
@@ -157,8 +157,9 @@ namespace rethus_backend.Models.Dto.UserForm
         public string AcademicsOriginTitle { get; set; }
         public string AcademicsTypeInstitution { get; set; }
         public string AcademicsProgramType { get; set; }
-        public int AcademicsDepartmentInstitution { get; set; }
-        public int AcademicsMunicipalityInstitution { get; set; }
+        public string AcademicsCountryInstitution { get; set; }
+        public string AcademicsDepartmentInstitution { get; set; }
+        public string AcademicsMunicipalityInstitution { get; set; }
         public string AcademicsNameInstitution { get; set; }
         public string AcademicsProgramName { get; set; }
         public DateTime AcademicsDateInstitution { get; set; }
