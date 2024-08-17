70af67cf4295a25a531c245371099c6dd1a87bd5
Alvis <aherrera@munily.com>
2024-08-17 15:00:13 -0500

    feat: add new lib of convert html to pdf

--- Utilities/ConvertPdfService/ConvertPdfService.cs
+++ Utilities/ConvertPdfService/ConvertPdfService.cs
@@ -1,19 +1,17 @@
-using PuppeteerSharp;
+using HtmlToPdfMaster;
 
 public class ConverPdfService
 {
     public static async Task ConvertHtmlToPdf(string htmlContent, string outputPath)
     {
-        await new BrowserFetcher().DownloadAsync();
-        var launchOptions = new LaunchOptions { Headless = true };
-        using var browser = await Puppeteer.LaunchAsync(launchOptions);
-        using var page = await browser.NewPageAsync();
-
-        // Convertir HTML a PDF
-        await page.SetContentAsync(htmlContent);
-        byte[] pdfBytes = await page.PdfDataAsync();
-
-        // Guardar el PDF en el disco
-        File.WriteAllBytes(outputPath, pdfBytes);
+        try
+        {
+            var pdfBytes = HtmlConverter.FromHtmlString(htmlContent, 220, 335);
+            File.WriteAllBytes(outputPath, pdfBytes);
+        }
+        catch (System.Exception)
+        {
+            Console.WriteLine("Exepcion manejada!");
+        }
     }
 }
