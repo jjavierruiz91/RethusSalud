56fefd0749506dd019b547941f097e6f0244ed9f
Alvis <aherrera@munily.com>
2025-05-15 17:20:32 -0500

    fix: revert password regex to exclude special characters for validation

--- Utilities/Security/password/password.cs
+++ Utilities/Security/password/password.cs
@@ -5,7 +5,7 @@ namespace rethus_backend.Utilities.Security.Hashing
     public class Password
     {
         private static readonly Regex PasswordRegex = new Regex(
-            @"^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$"
+            @"^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,}$"
         );
 
         public static bool ValidatePassword(string password, string confirmedPassword)
